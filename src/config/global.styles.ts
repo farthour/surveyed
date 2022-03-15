6f26dcb2c6e7f59f3e653e25b730738df26c0379
Amit Dimri <dimriamit98@gmail.com>
2022-03-15 16:19:57 +0530

    removed global styles

--- src/config/global.styles.ts
+++ src/config/global.styles.ts
@@ -1,12 +1,12 @@
-import { createGlobalStyle } from "styled-components";
+// import { createGlobalStyle } from "styled-components";
 import "./fonts/styles.css";
 
-export const GlobalStyles = createGlobalStyle`
-    * {
-        box-sizing: border-box;
-        margin: 0;
-        padding: 0;
+// export const GlobalStyles = createGlobalStyle`
+//     * {
+//         box-sizing: border-box;
+//         margin: 0;
+//         padding: 0;
 
-        font-family: "Montserrat", sans-serif;
-    }
-`;
+//         font-family: "Montserrat", sans-serif;
+//     }
+// `;
